11f80963034a59c10bfbc61c768fd49b0397517c
Kazuya Takei <myself@attakei.net>
2024-10-17 03:37:36 +0900

    fix: Use rollup to build as UDM(es5)

--- src/atsphinx/revealjs_rtd/static/atsphinx-revealjs-rtd/plugin.js
+++ src/atsphinx/revealjs_rtd/static/atsphinx-revealjs-rtd/plugin.js
@@ -3,6 +3,7 @@ const Plugin = {
   init(reveal) {
     const deck = reveal;
     deck.addKeyBinding({keyCode: 68, key: 'D', description: 'Toggle RTD widget'}, function() {
+      console.debug("Press toggle-key of RTD.");
       const flyout = document.querySelector("readthedocs-flyout");
       if (!flyout) return;
       flyout.style.display = flyout.style.display ? "" : "none";
