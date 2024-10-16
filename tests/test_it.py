d7a1865e0d1322e91bad1c370ba87b725c63e55c
Kazuya Takei <myself@attakei.net>
2024-10-17 01:52:59 +0900

    feat: Insert plugin js on build html (js is stub)

--- tests/test_it.py
+++ tests/test_it.py
@@ -1,6 +1,7 @@
 """Standard tests."""
 
 import pytest
+from bs4 import BeautifulSoup
 from sphinx.testing.util import SphinxTestApp
 
 
@@ -8,3 +9,11 @@ from sphinx.testing.util import SphinxTestApp
 def test__it(app: SphinxTestApp):
     """Test to pass."""
     app.build()
+    assert (app.outdir / "_static/atsphinx-revealjs-rtd/rtd.js").exists()
+    soup = BeautifulSoup((app.outdir / "index.html").read_text(), "html.parser")
+    scripts = [
+        s
+        for s in soup.find_all("script")
+        if "src" in s.attrs and s["src"].endswith("atsphinx-revealjs-rtd/rtd.js")
+    ]
+    assert scripts
