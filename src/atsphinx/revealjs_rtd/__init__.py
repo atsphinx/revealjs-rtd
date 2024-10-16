62a209f0ff08dece586dd0592079bf18a5466adb
Kazuya Takei <myself@attakei.net>
2024-10-17 01:35:30 +0900

    feat: Dpend and auto loading sphinx-revealjs

--- src/atsphinx/revealjs_rtd/__init__.py
+++ src/atsphinx/revealjs_rtd/__init__.py
@@ -6,6 +6,7 @@ __version__ = "0.0.0"
 
 
 def setup(app: Sphinx):  # noqa: D103
+    app.setup_extension("sphinx_revealjs")
     return {
         "version": __version__,
         "env_version": 1,
--- tests/test_it.py
+++ tests/test_it.py
@@ -4,7 +4,7 @@ import pytest
 from sphinx.testing.util import SphinxTestApp
 
 
-@pytest.mark.sphinx("html")
+@pytest.mark.sphinx("revealjs")
 def test__it(app: SphinxTestApp):
     """Test to pass."""
     app.build()
