3c99a43983a68affebe0dc8939cd3e8d3fba0b1d
Kazuya Takei <myself@attakei.net>
2024-10-17 02:34:34 +0900

    feat: Define toggle key

--- src/atsphinx/revealjs_rtd/static/atsphinx-revealjs-rtd/rtd.js
+++ src/atsphinx/revealjs_rtd/static/atsphinx-revealjs-rtd/rtd.js
@@ -1,6 +1,11 @@
 window.RevealRTD = window.RevealRTD || {
   id: 'rtd',
-  init() {
-    return;
+  init(reveal) {
+    const deck = reveal;
+    deck.addKeyBinding({keyCode: 68, key: 'D', description: 'Toggle RTD widget'}, function() {
+      const flyout = document.querySelector("readthedocs-flyout");
+      if (!flyout) return;
+      flyout.style.display = flyout.style.display ? "" : "none";
+    });
   }
 }
